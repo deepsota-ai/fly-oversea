7d57a29aee7d736a3f9b4fd705bc68d0a02b60bb
jiaweizhong <oracleinscience@gmail.com>
2026-07-18 13:33:55 -0700

    Refresh start

--- src/app/[lang]/(plain-layout)/pages/landing/_data/header-navigation.ts
+++
@@ -1,6 +0,0 @@
-export const headerNavigationData = [
-  { label: "首页", href: "" },
-  { label: "导师团队", href: "/consultants" },
-  { label: "学生案例", href: "/cases" },
-  { label: "常见问题", href: "/faq" },
-]
